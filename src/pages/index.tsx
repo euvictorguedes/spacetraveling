ad8475a04a8688840db601528da37c08922ba96a
Victor Guedes <victorfelipe.guedes@hotmail.com>
2022-04-18 22:06:12 -0300

    :children_crossing: Add Load More Posts function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Prismic from '@prismicio/client';
 import { format as dateFormat } from 'date-fns';
 import { FiCalendar, FiUser } from 'react-icons/fi';
 
+import { useState } from 'react';
 import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
 
@@ -33,6 +34,22 @@ interface HomeProps {
 export default function Home({
   postsPagination,
 }: HomeProps): React.ReactElement {
+  const [posts, setPosts] = useState<PostPagination>(postsPagination);
+
+  async function loadMorePosts(): Promise<void> {
+    const response = await fetch(`${posts.next_page}`).then(data =>
+      data.json()
+    );
+
+    const newPosts = {
+      ...posts,
+      next_page: response.next_page,
+      results: [...posts.results, ...response.results],
+    };
+
+    setPosts(newPosts);
+  }
+
   return (
     <>
       <Head>
@@ -43,14 +60,18 @@ export default function Home({
         <Header />
 
         <div className={styles.posts}>
-          {postsPagination.results.map(post => (
-            <Link href={`/posts/${post.uid}`} key={post.uid}>
+          {posts.results.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
               <a key={post.uid}>
                 <strong>{post.data.title}</strong>
                 <p>{post.data.subtitle}</p>
                 <div>
                   <time>
-                    <FiCalendar /> {post.first_publication_date}
+                    <FiCalendar />
+                    {dateFormat(
+                      new Date(post.first_publication_date),
+                      'dd LLL yyyy'
+                    ).toLowerCase()}
                   </time>
                   <span>
                     <FiUser /> {post.data.author}
@@ -61,8 +82,12 @@ export default function Home({
           ))}
         </div>
 
-        {postsPagination.next_page && (
-          <button type="button" className={styles.loadButton}>
+        {posts.next_page && (
+          <button
+            type="button"
+            className={styles.loadButton}
+            onClick={loadMorePosts}
+          >
             Carregar mais posts
           </button>
         )}
@@ -74,25 +99,15 @@ export default function Home({
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   const prismic = getPrismicClient();
   const postsResponse = await prismic.query(
-    Prismic.predicates.at('document.type', 'post')
+    Prismic.predicates.at('document.type', 'post'),
+    {
+      pageSize: 2,
+    }
   );
 
   const postsPagination: PostPagination = {
     next_page: postsResponse.next_page,
-    results: postsResponse.results.map(post => {
-      return {
-        uid: post.uid,
-        first_publication_date: dateFormat(
-          new Date(post.last_publication_date),
-          'dd LLL yyyy'
-        ),
-        data: {
-          title: post.data.title,
-          subtitle: post.data.subtitle,
-          author: post.data.author,
-        },
-      };
-    }),
+    results: postsResponse.results,
   };
 
   return {
