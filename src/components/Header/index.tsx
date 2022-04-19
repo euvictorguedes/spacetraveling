4a2f1cf5c679b4b7e3121256c039e5481709a350
Victor Guedes <victorfelipe.guedes@hotmail.com>
2022-04-18 22:03:17 -0300

    :lipstick: Add stylings for Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,10 +4,12 @@ import styles from './header.module.scss';
 
 export default function Header(): React.ReactElement {
   return (
-    <Link href="/">
-      <a>
-        <img src="/images/logo.svg" alt="logo" className={styles.logo} />
-      </a>
-    </Link>
+    <div className={styles.header}>
+      <Link href="/">
+        <a>
+          <img src="/images/logo.svg" alt="logo" className={styles.logo} />
+        </a>
+      </Link>
+    </div>
   );
 }
